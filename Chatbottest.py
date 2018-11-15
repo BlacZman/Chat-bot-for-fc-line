38be06117465692b4b534253e2b2112181401b25
BlacZman <37856140+BlacZman@users.noreply.github.com>
2018-11-15 13:29:52 +0700

    V1.1

--- Chatbottest.py
+++ Chatbottest.py
@@ -6,6 +6,15 @@ import http.client as user
 from credit import Account
 from datetime import datetime,date,timedelta
 
+class Cook:
+    def Writer(self, filename):
+        with open(filename, mode='w+') as file:
+            pass
+
+    def Reader(self, filename):
+        with open(filename) as file:
+            pass
+
 
 class Somchai(Client):
     def apiaiCon(self):
@@ -15,15 +24,27 @@ class Somchai(Client):
         self.request.lang = "de"
         self.request.session_id = "ggwp"
 
-    def WeatherConnector(self, receivedDate, location):
+
+    def WeatherConnector(self, obj):
         """
         Time forecast are 1:00, 4:00, 7:00, 10:00, 13:00, 16:00, 19:00, 22:00.
         Format date sent by Apiai are xxxx-xx-xx(10), xx:xx:xx(8), xxxx-xx-xx/xxxx-xx-xx(21), xx:xx:xx/xx:xx:xx(17),
         xxxx-xx-xxTxx:xx:xxZ, xxxx-xx-xxTxx:xx:xxZ/xxxx-xx-xxTxx:xx:xxZ
         check length of the date
         """
+        receivedDate = obj['result']['parameters']['date-time']
+        try:
+            location = obj['result']['parameters']['address']['business-name']
+        except:
+            print("No 'business-name' key in data!")
+            try:
+                location = obj['result']['parameters']['address']['city']
+            except:
+                print("No 'city' key in data!")
+                location = "Bangkok"
+        print(location)
+
         timeNow = datetime.now()
-        forecastTime = timeNow.replace(hour=1,minute=0,second=0, microsecond=0)
 
         if len(receivedDate) == 10:
             objTime = datetime.strptime(receivedDate, '%Y-%m-%d')
@@ -39,14 +60,31 @@ class Somchai(Client):
             objTime2 = datetime.strptime(date2, '%Y-%m-%d')
             deltaT = objTime2 - objTime1
             timeModify = timeNow + deltaT
-            timeModify = timeNow.timestamp()
 
-        #elif len(receivedDate) == 21:
+        elif len(receivedDate) == 17:
+            date1 = receivedDate[0:8]
+            date2 = receivedDate[9:17]
+            objTime1 = datetime.strptime(date1, '%H:%M:%S')
+            objTime2 = datetime.strptime(date2, '%H:%M:%S')
+            if objTime2 > objTime1:
+                objTime2 = objTime2.replace(year=timeNow.year, month=timeNow.month, day=timeNow.day)
+            else:
+                nextday = timeNow.day + 1
+                objTime2 = objTime2.replace(year=timeNow.year, month=timeNow.month, day=nextday)
 
+            objTime1 = objTime1.replace(year=timeNow.year, month=timeNow.month, day=timeNow.day)
+            timeNow = timeNow.replace(microsecond=0)
+            # check if base case is current time
+            if timeNow - objTime1  >= timedelta(seconds=2):
+                return 1
+
+            deltaT = objTime2 - objTime1
+            timeModify = timeNow + deltaT
 
         elif receivedDate == '':
-            receivedDate = timeNowq
+            receivedDate = timeNow
             deltaT = timedelta(days=0)
+
 
         print(deltaT)
 
@@ -61,8 +99,12 @@ class Somchai(Client):
             codeStatus = int(weatherObj['cod'])
             if codeStatus == 200:
                 print(weatherObj)
+                if obj['result']['action'] == 'weather':
+                    return [weatherObj['weather'][0]['description'],location]
+                else:
+                    return [weatherObj['main']['temp'],location]
             elif codeStatus == 404:
-                pass
+                print("Cannot find the city")
             conn.close()
 
         else:
@@ -79,28 +121,24 @@ class Somchai(Client):
         response = self.request.getresponse()
         obj = json.load(response)
         print(obj)
-        Location = "Bangkok"
         Action = obj['result']['action']
         reply = obj['result']['fulfillment']['speech']
         if author_id != self.uid:
             if Action == "weather" or Action == "weather.temperature":
-                dateInWeather = obj['result']['parameters']['date-time']
-                try:
-                    Location = obj['result']['parameters']['address']['business-name']
-                except:
-                    print("No 'business-name' key in data!")
-                    try:
-                        Location = obj['result']['parameters']['address']['city']
-                    except:
-                        print("No 'city' key in data!")
-                        Location = "Bangkok"
-                print(Location)
-                Result = self.WeatherConnector(dateInWeather, Location)
-                print(Location)
+                Result = self.WeatherConnector(obj)
                 if Result == 1:
-                    self.send(Message(text="Sorry, we cannot collect data from the past or forecast more than 5 days"), thread_id=thread_id, thread_type=thread_type)
+                    self.send(Message(text="Sorry, we cannot collect data from the past or forecast more than 5 days"),
+                              thread_id=thread_id, thread_type=thread_type)
                     self.markAsDelivered(author_id, thread_id)
-
+                else:
+                    if Action == "weather":
+                        self.send(Message(text="There are {} in {} today!!".format(Result[0],Result[1])),
+                              thread_id=thread_id, thread_type=thread_type)
+                        self.markAsDelivered(author_id, thread_id)
+                    else:
+                        self.send(Message(text="Today temperature is {} Celsius in {}!".format(Result[0],Result[1])),
+                                  thread_id=thread_id, thread_type=thread_type)
+                        self.markAsDelivered(author_id, thread_id)
             else:
                 self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
                 self.markAsDelivered(author_id, thread_id)
